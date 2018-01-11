d42f3116872e80d89b739e6cce20afccb2d5e0bb
Emilien Buton <emilien.buton@gmail.com>
2018-01-11 22:47:30 +0000

    Add user management API

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response, abort
 import json
 import sqlite3
 
 app = Flask(__name__)
 
+##### Info API
 @app.route ("/api/v1/info")
 def home_index():
     conn = sqlite3.connect('mydb.db')
@@ -19,6 +20,123 @@ def home_index():
         app_list.append(a_dict)
     conn.close()
     return jsonify({'api_version': app_list}), 200
+
+##### Users management
+## Get
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return list_users()
+
+def list_users():
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfuly");
+    api_list=[]
+    cursor = conn.execute("SELECT username, full_name, email, password, id from users")
+    for row in cursor:
+        a_dict = {}
+        a_dict['username'] = row[0]
+        a_dict['name'] = row[1]
+        a_dict['email'] = row[2]
+        a_dict['password'] = row[3]
+        a_dict['id'] = row[4]
+        api_list.append(a_dict)
+    conn.close()
+    return jsonify({'user_list': api_list})
+
+## Post
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = {
+        'username': request.json ['username'],
+        'email': request.json['email'],
+        'name': request.json.get('name',""),
+        'password': request.json['password']
+    }
+    return jsonify({'status': add_user(user)}), 201
+
+def add_user(new_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? or email=?",(new_user['username'],new_user['email']))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        abort(409)
+    else:
+       cursor.execute("insert into users (username, email, password, full_name) values(?,?,?,?)",(new_user['username'],new_user['email'], new_user['password'], new_user['name']))
+       conn.commit()
+       return "Success"
+    conn.close()
+    return jsonify(a_dict)
+
+## Delete
+@app.route('/api/v1/users', methods=['DELETE'])
+def delete_user():
+    if not request.json or not 'username' in request.json:
+        abort(400)
+    user=request.json['username']
+    return jsonify({'status': del_user(user)}), 200
+
+def del_user(del_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? ",(del_user,))
+    data = cursor.fetchall()
+    print ("Data, ",data)
+    if len(data) == 0:
+        abort(404)
+    else:
+        cursor.execute("DELETE from users where username==?",(del_user,))
+        conn.commit()
+        return "Success"
+
+## PUT
+@app.route('/api/v1/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = {}
+    if not request.json:
+        abort(400)
+    user['id']=user_id
+    key_list = request.json.keys()
+    for i in key_list:
+        user[i] = request.json[i]
+    print (user)
+    return jsonify({'status': update_user(user)}), 200
+
+def update_user(user):
+    conn=sqlite3.connect('mydb.db')
+    print("Opened database successfully");
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where id=? ",(user['id'],))
+    data = cursor.fetchall()
+    print ("Data, ",data)
+    if len(data) == 0:
+        abort(404)
+    else:
+        key_list=user.keys()
+        for i in key_list:
+            if i != "id":
+                print (user, i)
+                cursor.execute("""UPDATE users SET {0} = ? WHERE id = ?""".format(i), (user[i], user['id']))
+                conn.commit()
+    return "Success"
+
+##### Error handlers
+@app.errorhandler(400)
+def invalid_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return make_response(jsonify({'error': 'Resource not found !'}), 404)
+
+@app.errorhandler(409)
+def user_found(error):
+    return make_response(jsonify({'error': 'Conflict! Record exist'}), 409)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
